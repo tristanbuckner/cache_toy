2a5c20e42bd6dcacfed64793c56a93620b7b7527
Tristan Buckner <tristan@Tristans-MacBook-Pro.local>
2020-05-04 18:30:05 -0700

    add comments

---
+++ README.md
@@ -0,0 +1,7 @@
+###### Toy Bounded Cache Implementation
+Implements the constant time remove feature we discussed at the end of the phone screen.
+
+To run tests and generate a code coverage report:
+```./gradlew jacocoTestReport```
+
+Report will be at `build/jacoco-report/test/html/index.html`
--- src/main/java/Cache.java
+++ src/main/java/Cache.java
@@ -2,14 +2,17 @@ import java.util.HashMap;
 import java.util.Optional;
 
 public class Cache<K, V> {
-    private int capacity;
+    private int maxSize;
     private HashMap<K, Node<K, V>> nodes = new HashMap<>();
     private Node<K, V> oldest;
     private Node<K, V> newest;
 
-
-    public Cache(int capacity) {
-        this.capacity = capacity;
+    /**
+     * Constructs a bounded cache with the supplied max size
+     * @param maxSize
+     */
+    public Cache(int maxSize) {
+        this.maxSize = maxSize;
     }
 
     synchronized public void put(K key, V value) {
@@ -19,10 +22,12 @@ public class Cache<K, V> {
 
             Node<K, V> node = new Node<>(key, value);
 
+            //Set both to the first node if the cache is empty, perform an eviction if the max size is reached, or
+            // just add the node
             if (nodes.size() == 0) {
                 oldest = node;
                 newest = node;
-            } else if (nodes.size() == capacity) {
+            } else if (nodes.size() == maxSize) {
                 K keyToRemove = popOldest().key;
                 nodes.remove(keyToRemove);
                 addNewest(node);
@@ -35,11 +40,21 @@ public class Cache<K, V> {
         }
     }
 
+    /**
+     * Get the value corresponding to the supplied key or return null
+     * @param key
+     * @return
+     */
     synchronized public V get(K key) {
         return Optional.ofNullable(nodes.get(key)).map(Node::getValue).orElse(null);
     }
 
+    /**
+     * Use the nodes map to jump to the desired key in constant time and detach it from the linked list
+     * @param key
+     */
     synchronized public void remove(K key) {
+        //Clean up newest and oldest when removing all items
         if (newest != null && newest.key.equals(key)) {
             newest = newest.older;
         }
@@ -50,12 +65,16 @@ public class Cache<K, V> {
 
         if (nodes.containsKey(key)) {
             Node<K, V> oldNode = nodes.get(key);
-            oldNode.delink();
+            oldNode.deLink();
             nodes.remove(key);
 
         }
     }
 
+    /**
+     * Pop the oldest element and detach the reference from the adjacent entry
+     * @return the oldest element in the cache
+     */
     private Node<K, V> popOldest() {
         Node<K, V> returnValue = oldest;
 
@@ -65,6 +84,10 @@ public class Cache<K, V> {
         return returnValue;
     }
 
+    /**
+     * Add a new node and make the previous newest node point to this new node
+     * @param newNode
+     */
     private void addNewest(Node<K, V> newNode) {
         newNode.setOlder(newest);
         newest.setNewer(newNode);
@@ -75,10 +98,16 @@ public class Cache<K, V> {
     @Override
     public String toString() {
         return "Cache{" +
-                "capacity=" + capacity +
+                "capacity=" + maxSize +
                 ", pairs new to old=[" + (newest != null ? newest.newToOld() : "empty") + "]" +
                 '}';
     }
+
+    /**
+     * Simple Node for doubly linked list operations for cache evictions
+     * @param <K>
+     * @param <V>
+     */
 
     private static class Node<K, V> {
         private Node<K, V> older;
@@ -111,7 +140,7 @@ public class Cache<K, V> {
             return value;
         }
 
-        public void delink() {
+        public void deLink() {
             if (older != null ) {
                 older.setNewer(newer);
             }
