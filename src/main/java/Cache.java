e276f00f0675baa9c66f5d0c414d364e76454ffa
Tristan Buckner <tristan@Tristans-MacBook-Pro.local>
2020-05-04 17:35:12 -0700

    first commit

---
+++ src/main/java/Cache.java
@@ -0,0 +1,133 @@
+import java.util.HashMap;
+import java.util.Optional;
+
+public class Cache<K, V> {
+    private int capacity;
+    private HashMap<K, Node<K, V>> nodes = new HashMap<>();
+    private Node<K, V> oldest;
+    private Node<K, V> newest;
+
+
+    public Cache(int capacity) {
+        this.capacity = capacity;
+    }
+
+    synchronized public void put(K key, V value) {
+        if (nodes.containsKey(key)) {
+            nodes.get(key).setValue(value);
+        } else {
+
+            Node<K, V> node = new Node<>(key, value);
+
+            if (nodes.size() == 0) {
+                oldest = node;
+                newest = node;
+            } else if (nodes.size() == capacity) {
+                K keyToRemove = popOldest().key;
+                nodes.remove(keyToRemove);
+                addNewest(node);
+            } else {
+                addNewest(node);
+            }
+
+            nodes.put(key, node);
+
+        }
+    }
+
+    synchronized public V get(K key) {
+        return Optional.ofNullable(nodes.get(key)).map(Node::getValue).orElse(null);
+    }
+
+    synchronized public void remove(K key) {
+        if (newest != null && newest.key.equals(key)) {
+            newest = newest.older;
+        }
+
+        if (oldest != null && oldest.key.equals(key)) {
+            oldest = oldest.newer;
+        }
+
+
+        if (nodes.containsKey(key)) {
+            Node<K, V> oldNode = nodes.get(key);
+            oldNode.delink();
+            nodes.remove(key);
+
+        }
+    }
+
+    private Node<K, V> popOldest() {
+        Node<K, V> returnValue = oldest;
+
+        oldest = oldest.newer;
+        oldest.setOlder(null);
+
+        return returnValue;
+    }
+
+    private void addNewest(Node<K, V> newNode) {
+        newNode.setOlder(newest);
+        newest.setNewer(newNode);
+        newest = newNode;
+
+    }
+
+
+    private static class Node<K, V> {
+        private Node<K, V> older;
+        private Node<K, V> newer;
+        private final K key;
+        private V value;
+
+        public Node(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "left=" + (older != null ? older.key.toString(): "null") +
+                    ", key=" +  key +
+                    ", value=" + value +
+                    ", right=" + (newer != null ? newer.key.toString(): "null") +
+                    '}';
+        }
+
+        public String newToOld() {
+            return key + " -> " + Optional.ofNullable(older).map(Node::newToOld).orElse("end");
+        }
+
+        public String oldToNew() {
+            return key + " -> " + Optional.ofNullable(newer).map(Node::oldToNew).orElse("end");
+        }
+
+        public void setOlder(Node<K, V> older) {
+            this.older = older;
+        }
+
+        public void setNewer(Node<K, V> newer) {
+            this.newer = newer;
+        }
+
+        public void setValue(V value) {
+            this.value = value;
+        }
+
+        public V getValue() {
+            return value;
+        }
+
+        public void delink() {
+            if (older != null ) {
+                older.setNewer(newer);
+            }
+
+            if (newer != null) {
+                newer.setOlder(older);
+            }
+        }
+
+    }
+}
---
+++ src/test/java/CacheTest.java
@@ -0,0 +1,85 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+public class CacheTest {
+
+    @Test
+    public void putGetTest() {
+        var cache = new Cache<Integer, String>(10);
+
+        cache.put(1234, "test");
+
+        assertEquals("test", cache.get(1234));
+
+    }
+
+    @Test
+    public void evictAtCapacityTest() {
+        var cache = new Cache<Integer, String>(2);
+
+        cache.put(1234, "test1");
+        cache.put(1235, "test2");
+        cache.put(1236, "test3");
+
+
+        assertNull(cache.get(1234));
+        assertEquals("test2", cache.get(1235));
+        assertEquals("test3", cache.get(1236));
+    }
+
+
+    @Test
+    public void removalTest() {
+        var cache = new Cache<Integer, String>(3);
+
+        cache.put(1234, "test1");
+        cache.put(1235, "test2");
+        cache.put(1236, "test3");
+
+        cache.remove(1235);
+
+        assertEquals("test1", cache.get(1234));
+        assertNull(cache.get(1235));
+        assertEquals("test3", cache.get(1236));
+    }
+
+    @Test
+    public void removalAllTest() {
+        var cache = new Cache<Integer, String>(3);
+
+        cache.put(1234, "test1");
+        cache.put(1235, "test2");
+        cache.put(1236, "test3");
+
+        cache.remove(1234);
+        cache.remove(1235);
+        cache.remove(1236);
+
+        cache.put(1234, "test1");
+
+        assertEquals("test1", cache.get(1234));
+        assertNull(cache.get(1235));
+        assertNull(cache.get(1236));
+    }
+
+    @Test
+    public void overWriteTest() {
+        var cache = new Cache<Integer, String>(3);
+
+        cache.put(1234, "test1");
+        cache.put(1234, "test2");
+
+        assertEquals("test2", cache.get(1234));
+
+    }
+
+    @Test
+    public void removeNonPresent() {
+        var cache = new Cache<Integer, String>(3);
+        cache.remove(1234);
+    }
+
+}
